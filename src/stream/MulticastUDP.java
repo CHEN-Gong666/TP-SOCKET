dbac755def1040a591a0cf9736278c0285642773
hmy <hmy825018653@gmail.com>
2020-11-23 06:00:16 +0100

    feat:IHM of udp

--- src/stream/MulticastUDP.java
+++ src/stream/MulticastUDP.java
@@ -1,13 +1,31 @@
 package stream;
 
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.*;
 import java.util.LinkedList;
 
-public class MulticastUDP {
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+
+
+public class MulticastUDP extends JFrame {
+
     private static LinkedList<String> chatHistory = new LinkedList<>();
+
+
+    static JTextArea Name;
+    static JTextArea TextOut;
+    static JTextField TextIn;
+    static JPanel Panel;
+    static JButton Send;
+    static JButton ChangeName;
 
     public static void main(String[] args) throws IOException {
 
@@ -26,6 +44,48 @@ public class MulticastUDP {
         }
 
         try {
+            JFrame f =new JFrame();
+            f.setSize(550,440);
+            Panel = new JPanel();
+            f.add(Panel);
+
+
+            TextOut = new JTextArea(20,45);
+            TextOut.setBorder(BorderFactory.createLineBorder(Color.gray,5));
+            TextOut.setText("welcome to the chat system");
+            TextOut.setEditable(false);
+
+            //Monitor mo = new Monitor();
+            Send = new JButton("send");
+            Send.addActionListener(
+                    new ActionListener()
+                    {
+                        public void actionPerformed(ActionEvent e)
+                        {
+                            TextIn.setText("");
+                        }
+                    });
+
+            ChangeName = new JButton("Change Name");
+            ChangeName.addActionListener(
+                    new ActionListener()
+                    {
+                        public void actionPerformed(ActionEvent e)
+                        {
+                        }
+                    });
+
+            TextIn = new JTextField(39);
+            Name = new JTextArea(1,15);
+            Name.setText("pseudo");
+
+            Panel.add(TextOut);
+            Panel.add(TextIn);
+            Panel.add(Send);
+            Panel.add(Name);
+            Panel.add(ChangeName);
+            f.setVisible(true);
+
             stdIn = new BufferedReader(new InputStreamReader(System.in));
             System.out.print( "Please enter your name : " );
             clientName = stdIn.readLine();
@@ -44,6 +104,8 @@ public class MulticastUDP {
             String connexionMsg = clientName + " joins the chat";
             msg = new DatagramPacket(connexionMsg.getBytes(),connexionMsg.length(),groupAddr,groupPort);
             socket.send(msg);
+
+
         } catch (UnknownHostException e) {
             System.err.println("Don't know about host:" + args[0]);
             System.exit(1);
@@ -98,4 +160,8 @@ public class MulticastUDP {
 
     }
 
+
+
+
 }
+
