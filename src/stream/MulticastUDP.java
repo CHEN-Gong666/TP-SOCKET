6b4247d4c591c53adaf0317c44822800dcda9b83
hmy <hmy825018653@gmail.com>
2020-11-23 15:59:02 +0100

    fix:IHM of udp,now workable

--- src/stream/MulticastUDP.java
+++ src/stream/MulticastUDP.java
@@ -15,10 +15,9 @@ import javax.swing.*;
 import javax.swing.border.Border;
 
 
-public class MulticastUDP extends JFrame {
+public class MulticastUDP extends JFrame{
 
     private static final LinkedList<String> chatHistory = new LinkedList<>();
-
 
     static JTextArea Name;
     static JTextArea TextOut;
@@ -26,24 +25,31 @@ public class MulticastUDP extends JFrame {
     static JPanel Panel;
     static JButton Send;
     static JButton ChangeName;
+    static JScrollPane Scroll;
+    String clientName = "pseudo";
+    String line;
+    InetAddress groupAddr;
+    int groupPort = 50000;
+    MulticastSocket socket = null;
+    DatagramPacket msg;
 
     public static void main(String[] args) throws IOException {
+        MulticastUDP aUDP = new MulticastUDP();
 
-        MulticastSocket socket = null;
         BufferedReader stdIn = null;
-        InetAddress groupAddr = InetAddress.getByName("224.0.0.233");
-        int groupPort = 50000;
-        String clientName = "peuso";
-        String line;
-        DatagramPacket msg;
+        aUDP.groupAddr = InetAddress.getByName("224.0.0.233");
+        //int groupPort = 50000;
+
+        //DatagramPacket msg;
         ClientThreadUDP clientThreadUDP = null;
 
         if (args.length == 2) {
-            groupAddr = InetAddress.getByName(args[0]);
-            groupPort = new Integer(args[1]).intValue();
+            aUDP.groupAddr = InetAddress.getByName(args[0]);
+            aUDP.groupPort = new Integer(args[1]).intValue();
         }
 
         try {
+
             JFrame f =new JFrame();
             f.setSize(550,440);
             Panel = new JPanel();
@@ -52,48 +58,47 @@ public class MulticastUDP extends JFrame {
 
             TextOut = new JTextArea(20,45);
             TextOut.setBorder(BorderFactory.createLineBorder(Color.gray,5));
-            TextOut.setText("welcome to the chat system");
+            TextOut.setText("welcome to the chat system\n");
             TextOut.setEditable(false);
+            JScrollPane Scroll = new JScrollPane(TextOut);
+            Scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 
-            //Monitor mo = new Monitor();
             Send = new JButton("send");
-            Send.addActionListener(
-                    new ActionListener()
-                    {
-                        public void actionPerformed(ActionEvent e)
-                        {
-                            TextIn.setText("");
-                        }
-                    });
+            Send.addActionListener(new MyMonitor(aUDP));
 
             ChangeName = new JButton("Change Name");
-            ChangeName.addActionListener(
-                    new ActionListener()
+            ChangeName.addActionListener(new MyMonitor(aUDP));
+        /*ChangeName.addActionListener(
+                new ActionListener()
+                {
+                    public void actionPerformed(ActionEvent e)
                     {
-                        public void actionPerformed(ActionEvent e)
-                        {
-                        }
-                    });
+                        JTextArea nta = (JTextArea)e.getSource();
+                    }
+                });
+        */
 
             TextIn = new JTextField(39);
             Name = new JTextArea(1,15);
             Name.setText("pseudo");
 
-            Panel.add(TextOut);
+            //Panel.add(TextOut);
+            Panel.add(Scroll);
             Panel.add(TextIn);
             Panel.add(Send);
             Panel.add(Name);
             Panel.add(ChangeName);
+
             f.setVisible(true);
-
             stdIn = new BufferedReader(new InputStreamReader(System.in));
-            System.out.print( "Please enter your name : " );
-            clientName = stdIn.readLine();
 
-            socket = new MulticastSocket(groupPort);
-            socket.joinGroup(groupAddr);
+            TextOut.append("enter your user name\n");
+            //System.out.print( "Please enter your name : \n" );
+
+            aUDP.socket = new MulticastSocket(aUDP.groupPort);
+            aUDP.socket.joinGroup(aUDP.groupAddr);
             stdIn = new BufferedReader(new InputStreamReader(System.in));
-            clientThreadUDP = new ClientThreadUDP(socket);
+            clientThreadUDP = new ClientThreadUDP(aUDP.socket);
             clientThreadUDP.start();
 
             System.out.println(chatHistory);
@@ -101,9 +106,9 @@ public class MulticastUDP extends JFrame {
                 System.out.println(m);
             }
 
-            String connexionMsg = clientName + " joins the chat";
-            msg = new DatagramPacket(connexionMsg.getBytes(),connexionMsg.length(),groupAddr,groupPort);
-            socket.send(msg);
+            String connexionMsg = aUDP.clientName + " joins the chat\n";
+            aUDP.msg = new DatagramPacket(connexionMsg.getBytes(),connexionMsg.length(),aUDP.groupAddr,aUDP.groupPort);
+            aUDP.socket.send(aUDP.msg);
 
 
         } catch (UnknownHostException e) {
@@ -117,25 +122,22 @@ public class MulticastUDP extends JFrame {
 
         while (true) {
 
-            line =Send.getText();
-            if (line.equals(".")) {
-                String deconnexionMsg = clientName + " leaves the chat";
+            //aUDP.line =stdIn.readLine();
+            if (aUDP.line.equals(".")) {
+                String deconnexionMsg = aUDP.clientName + " leaves the chat";
                 chatHistory.add(deconnexionMsg);
-                msg = new DatagramPacket(deconnexionMsg.getBytes(),deconnexionMsg.length(),groupAddr,groupPort);
-                socket.send(msg);
-                socket.leaveGroup(groupAddr);
+                aUDP.msg = new DatagramPacket(deconnexionMsg.getBytes(),deconnexionMsg.length(),aUDP.groupAddr,aUDP.groupPort);
+                aUDP.socket.send(aUDP.msg);
+                aUDP.socket.leaveGroup(aUDP.groupAddr);
                 clientThreadUDP.stop();
                 System.out.println("before break");
                 break;
             }
 
-
-
-
-            String msgString = clientName + " : " + line;
-            msg = new DatagramPacket(msgString.getBytes(), msgString.length(), groupAddr, groupPort);
-            TextOut.append(clientName + " : " + line);
-            socket.send(msg);
+            //String msgString = aUDP.clientName + " : " + aUDP.line;
+            //aUDP.msg = new DatagramPacket(msgString.getBytes(), msgString.length(), aUDP.groupAddr, aUDP.groupPort);
+            //TextOut.append(clientName + " : " + line+'\n');
+            //aUDP.socket.send(aUDP.msg);
 
         }
 
@@ -168,8 +170,38 @@ public class MulticastUDP extends JFrame {
 
     }
 
-
-
-
 }
+
+class MyMonitor implements ActionListener {
+    MulticastUDP mf = null;
+    public MyMonitor(MulticastUDP ttmf) {
+        this.mf = ttmf;
+    }
+    public void actionPerformed(ActionEvent e) {
+        String buttonName = e.getActionCommand();
+        //System.out.println(buttonName);
+        if(buttonName.equals("Change Name")){
+            mf.clientName = mf.Name.getText();
+            mf.TextOut .append("You've changed your name to:"+mf.clientName+'\n');
+
+        }else {
+            if (buttonName.equals("send")) {
+                mf.line = mf.TextIn.getText();
+                mf.TextIn.setText("");
+                String msgString = mf.clientName + " : " + mf.line;
+                mf.msg = new DatagramPacket(msgString.getBytes(), msgString.length(), mf.groupAddr, mf.groupPort);
+                //mf.TextOut.append(mf.clientName + " : " + mf.line+'\n');
+                try {
+                    mf.socket.send(mf.msg);
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                }
+            } else {
+                JOptionPane.showMessageDialog(mf, "Unknown event" );
+            }
+        }
+    }
+}
+
+
 
